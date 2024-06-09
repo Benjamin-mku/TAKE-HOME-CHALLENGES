6592b71606a0aa9769da4cf1efa298326cd99790
Benjamin <imanirakomeyeb@gmail.com>
2024-06-09 18:01:18 +0300

    selecting each photo inside main js

---
+++ Galley Two/javascript/main.js
@@ -0,0 +1,7 @@
+//selecting each photo
+const photo_one = document.getElementById("photo_one");
+const photo_two = document.getElementById("photo_two");
+const photo_three = document.getElementById("photo_three");
+const photo_four = document.getElementById("photo_four");
+const photo_one_img = document.querySelector(".photo-one");
+const phome = document.getElementById("phome"); //id added to img-1
