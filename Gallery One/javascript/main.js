eff1e4f81381b1affb60e6bbfc076c62bd73f453
Benjamin <imanirakomeyeb@gmail.com>
2024-06-09 16:44:11 +0300

    selecting each photo

---
+++ Gallery One/javascript/main.js
@@ -0,0 +1,8 @@
+//selecting  each photo
+const photo1 = document.getElementById("photo1");
+const photo2 = document.getElementById("photo2");
+const photo3 = document.getElementById("photo3");
+const photo4 = document.getElementById("photo4");
+const caption = document.getElementById("caption");
+//selecting the container division
+const container = document.querySelector(".container");
