66bd98e3bd9b3a14ce8eabcb09be84a1ca42d7a3
Benjamin <imanirakomeyeb@gmail.com>
2024-06-09 18:05:33 +0300

    adding event listner of mouse over to photo 1 and creating new div element with classname of box and appending all childs to that div and also setting opacity of 0 to all other remaining photos

--- Galley Two/javascript/main.js
+++ Galley Two/javascript/main.js
@@ -5,3 +5,18 @@ const photo_three = document.getElementById("photo_three");
 const photo_four = document.getElementById("photo_four");
 const photo_one_img = document.querySelector(".photo-one");
 const phome = document.getElementById("phome"); //id added to img-1
+//Adding event listner to each photo
+
+//Event Listner for photo one ----------------------------------------
+photo_one.addEventListener("mouseover", () => {
+  //here we need to create the box that will show up whenever we hover on photo
+  const div = document.createElement("div");
+  //set classname of box to div element
+  div.className = "box1";
+  photo_one_img.appendChild(div);
+  //adding opacity of 0 to each photo whenever we hover to photo one
+  phome.style.opacity = 0; //extend id added to the img-1
+  photo_two.style.opacity = 0;
+  photo_three.style.opacity = 0;
+  photo_four.style.opacity = 0;
+});
